7a19f79b70eb987f19b62755bd9eaa3724c31873
XPH-Dev <admin@phantom.org>
2019-04-15 15:41:20 +0200

    change github

--- __tests__/unit/services/release.spec.js
+++ __tests__/unit/services/release.spec.js
@@ -5,7 +5,7 @@ import errorCapturer from '../__utils__/error-capturer'
 import release from '@/services/release'
 
 const releaseApiUrl = 'https://api.github.com/repos/PhantomCore/desktop-wallet/releases/latest'
-const releaseUrl = 'https://github.com/ArkEcosystem/desktop-wallet/releases/latest'
+const releaseUrl = 'https://github.com/PhantomChain/desktop-wallet/releases/latest'
 
 describe('Services > Release', () => {
   describe('latestReleaseApiUrl', () => {
